5c431f0004e746eafba8fa78d1bb1e3df1733461
Aritra Dutta <72460607+Aritra222@users.noreply.github.com>
2020-10-07 02:49:30 +0530

    Add files via upload

---
+++ basicpython.py
@@ -0,0 +1,63 @@
+#python 3.7.1
+
+print ("Hello, Guys!")
+aritra1= "My name is name John"
+print (len(aritra1))
+print (aritra1[0:11])
+print ( aritra1[:])
+print (aritra1[::2])
+print (aritra1.upper())
+print (aritra1.lower())
+print (aritra1.replace('a','e' ))
+print (aritra1.count("n"))
+print (aritra1.find("s"))
+print (aritra1.split(','))
+var1 = 10
+print (type(var1))
+var2 = 3-4j
+print (type(var2))
+var3 = 2.85
+print (type(var3))
+var4 = True
+print (type(var4))
+#Arithmetic operators
+# +,-,/,*
+a = 5
+b= 7
+print (a+b)
+print (a-b)
+print (a/b)
+print (a*b)
+#Relational operators
+# >,<,==,!=
+print (a>b)
+print (a<b)
+print (a==b)
+print (a!=b)
+x = '';
+print (x);
+#Logical operators
+#&,|
+c = True
+d = False
+print (c&d)
+print (c|d)
+print (d&c)
+print (d|c)
+print (c&c)
+print (c|c)
+print (d&d)
+print (d|d)
+print (x);
+print (x);
+list1 = ["Ram" , 5],["Sam" , 8],["John" , 10],["Carry",15],["Harry",20]
+for item,Roll in list1:
+  print (item, "'s Roll number is", Roll)
+print (x);
+print (x);
+
+tup1 = (1,3.98,4-8j,True)
+print (tup1)
+print (type(tup1))
+print (tup1[0:4])
+print (tup1[-1])
